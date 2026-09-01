5b64d88f1ab8382e52116b7b8529c634e8db45c2
emanuelly.rodrigues <emanuelly.rodrigues@dcx.ufpb.br>
2026-09-01 11:42:41 -0300

    Modificando o código para adicionar um método e operações

--- src/main/java/Oi.java
+++ src/main/java/Oi.java
@@ -1,7 +1,28 @@
+import javax.swing.JOptionPane;
+
 public class Oi {
 
-    public static void main(String [] args) {
-        System.out.println("Oi");
+    public static void mostreMensagemBoasVindas(String nome){
+        System.out.println("Oi "+ nome);
+        System.out.println("Como vai você?");
+    }
+
+    public static void main(String [] args){
+        String nomeLido = JOptionPane.showInputDialog("Diga seu nome");
+        mostreMensagemBoasVindas(nomeLido);
+
+        System.out.println(3+4);
+        System.out.println(3*4);
+        System.out.println(30.0/4);
+        System.out.println(15%2);
+        System.out.println(15-2);
+
+
+        //char letra = 'c';
+        //double altura = 1.75;
+        //float numeroReal = 8.87f;
+        //boolean toComFome = true;
 
     }
+
 }
